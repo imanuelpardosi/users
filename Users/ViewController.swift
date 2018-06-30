b3d3fe75170b5aadec89274fa70c5b404ecd7fed
Imanuel Pardosi <ipardosi@dyned.com>
2018-06-30 13:53:36 +0700

    Initial project

---
+++ Users/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Users
+//
+//  Created by Mobile Jakarta Team on 30/06/18.
+//  Copyright © 2018 moonshadow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
